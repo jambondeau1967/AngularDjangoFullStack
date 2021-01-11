e16b9f0d017f11b02a31282905d6390a3f899d9f
Hans Waterham <hans.waterham@me.com>
2021-01-11 11:44:03 +0100

    20210111-1
    bla+secretkey

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
  The dot causes to create the project in current folder, manage.py appears next to venv*
 project=first
 app=demo
+
+django-admin startproject first
+django-admin startproject demo
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
-# Create your views here.
+
+class Another(View):
+
+    books = Book.objects.all()
+    #books = Book.objects.filter(is_published=True)
+    # book = Book.objects.get(is_published=True) : just 1 record, for must be commented out.
+    # book = Book.objects.get(id=1) : just 1 record, for must be commented out.
+    lines = ''
+    lines += f'We have {len(books)} books in database:<br>'
+    lines += f'<br>'
+
+    for book in books:
+        thisbook = f'book: {book.title} with ID {book.id} in DB.<br>'
+        lines += thisbook
+
+    def get(self, request):
+        return HttpResponse(self.lines)
+
+
+def first(request):
+    return HttpResponse('First message from views')
