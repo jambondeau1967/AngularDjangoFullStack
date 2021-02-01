336012370277b22775863fee170ba88f685a3374
Hans Waterham <hans.waterham@me.com>
2021-02-01 21:28:56 +0100

    20210201
    20210201

--- section2_djangointro/demo/urls.py
+++ section2_djangointro/demo/urls.py
@@ -3,6 +3,8 @@ from rest_framework import routers
 from .views import BookViewSet
 
 router = routers.DefaultRouter()
+# register url
+# http://127.0.0.1:8000/demo/books/
 router.register('books', BookViewSet)
 
 urlpatterns = [
